46741946a05487f940cf0b19f308c1cfad4ddc8e
bruglesco <klitzinger@gmail.com>
2018-08-02 14:45:39 -0400

    created sorting comparators for custom class.

--- DataSorter/helper_functions.h
+++ DataSorter/helper_functions.h
@@ -12,8 +12,16 @@ struct student
 	student(std::string name, unsigned id, unsigned score) : name{ name }, id{ id }, score{ score } {}
 };
 
+
+
 void check_file(std::ifstream& source_file);
 
 void sort_data();
 
 void sort_three_ways(char choice);
+
+bool sort_by_name(const student& lhs, const student& rhs) { return lhs.name > rhs.name; }
+
+bool sort_by_id(const student& lhs, const student& rhs) { return lhs.id > rhs.id; }
+
+bool sort_by_score(const student& lhs, const student& rhs) { return lhs.score > rhs.score; }
