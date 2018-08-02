c2b3925154a7fcfd06203e51aed78e18330a5e1c
bruglesco <klitzinger@gmail.com>
2018-08-02 09:50:21 -0400

    Created struct for storing data

--- DataSorter/helper_functions.h
+++ DataSorter/helper_functions.h
@@ -1,5 +1,15 @@
 #pragma once
 
 #include <fstream>
+#include <string>
+
+struct student
+{
+	std::string name;
+	unsigned id;
+	unsigned score;
+
+	student(std::string name, unsigned id, unsigned score) : name{ name }, id{ id }, score{ score } {}
+};
 
 void check_file(std::ifstream& source_file);
