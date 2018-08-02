4f6f6d457cd1ae4fa1c3ee172a27f0e1ef416ed4
bruglesco <klitzinger@gmail.com>
2018-08-02 00:03:51 -0400

    Openned fstream and created function to check it.

---
+++ DataSorter/helper_functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "helper_functions.h"
+
+void check_file(std::ifstream& source_file)
+{
+	if (!source_file)
+	{
+		std::cerr << "Failure to load file. Please re-enter file name.\n";
+		std::cout << "Please enter a file name.\n";
+		std::string file_name;
+		std::cin >> file_name;
+		source_file.open(file_name);
+		check_file(source_file);
+	}
+}
---
+++ DataSorter/helper_functions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <fstream>
+
+void check_file(std::ifstream& source_file);
