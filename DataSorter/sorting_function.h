df424b2e4b198c01001c547c9a1e1169449a3dee
bruglesco <klitzinger@gmail.com>
2018-08-07 01:14:53 -0400

    Working on rewriting sort functions with iterator interface.
    -- status incomplete

---
+++ DataSorter/sorting_function.h
@@ -0,0 +1,14 @@
+#ifndef BRUGLESCO_DATASORTER_SORTING_FUNCTION_H
+#define BRUGLESCO_DATASORTER_SORTING_FUNCTION_H
+
+#include <vector>
+
+#include "helper_functions.h"
+
+void bubble_sort(std::vector<student> data, sort_type type);
+
+void bucket_sort(std::vector<student> data, std::vector<student>& sorted_data, sort_type type);
+
+void selection_sort(std::vector<student> data, sort_type type);
+
+#endif // !BRUGLESCO_DATASORTER_SORTING_FUNCTION_H
