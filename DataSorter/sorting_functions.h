33153539def032f4cc3acb684f88859565ce8c8d
bruglesco <klitzinger@gmail.com>
2018-08-13 22:09:41 -0400

    bug fix in bubble sort iterator access violation. bug fix in merge sort algorithm (need to pass through the whole collection)

--- DataSorter/sorting_functions.h
+++ DataSorter/sorting_functions.h
@@ -29,7 +29,7 @@ namespace bruglesco {
 			{
 				if (first_pass) { ++size; }
 				auto temp_lhs = temp_rhs++;
-				if (temp_lhs != end && func(*temp_lhs, *temp_rhs))
+				if (temp_rhs != end && func(*temp_lhs, *temp_rhs))
 				{
 					std::swap(*temp_lhs, *temp_rhs);
 				}
@@ -59,7 +59,7 @@ namespace bruglesco {
 		std::cout << "*\n";
 	}
 
-	// slightly bugged need to move value when rhs > lhs not swap.
+	// bugged. need to move not swap when rhs is smaller
 	template <typename Iterator, typename Func>
 	inline void merge_sort(Iterator begin, Iterator end, Func func = default_comparison)
 	{
@@ -67,9 +67,9 @@ namespace bruglesco {
 			"----------|Finished\n";
 
 		bool stopped{ false };
-
 		std::size_t sub_size = 1;
 		std::size_t size = 0;
+		std::size_t count = 0;
 
 		do
 		{
@@ -78,26 +78,48 @@ namespace bruglesco {
 			std::size_t left_count = sub_size;
 			std::size_t right_count = sub_size;
 
-			for (std::size_t i = 0; i < sub_size; ++i)
+			while (it_rhs != end)
 			{
-				++it_rhs;
-				if (sub_size == 1)
+				for (std::size_t i = 0; i < sub_size; ++i)
 				{
-					++size;
+					++it_rhs;
+					if (sub_size == 1)
+					{
+						++size;
+					}
+					if (it_rhs == end)
+					{
+						break;
+					}
 				}
-			}
-
-			while (left_count > 0 && right_count > 0)
-			{
-				if (it_rhs == end || func(*it_lhs, *it_rhs))
+				while (left_count > 0 && right_count > 0)
 				{
-					--left_count;
+					if (it_rhs == end || func(*it_lhs, *it_rhs))
+					{
+						--left_count;
+						++it_lhs;
+					}
+					else
+					{
+						std::swap(*it_lhs, *it_rhs);
+						--right_count;
+						++it_rhs;
+						if (sub_size == 1)
+						{
+							++size;
+						}
+					}
+				}
+				for (std::size_t i = 0; i < left_count; ++i)
+				{
 					++it_lhs;
 				}
-				else
+				for (std::size_t i = 0; i < right_count; ++i)
 				{
-					std::swap(*it_lhs, *it_rhs);
-					--right_count;
+					if (it_rhs == end)
+					{
+						break;
+					}
 					++it_rhs;
 					if (sub_size == 1)
 					{
@@ -106,26 +128,24 @@ namespace bruglesco {
 				}
 			}
 
-			for (std::size_t i = 0; i < left_count; ++i)
-			{
-				++it_lhs;
-			}
-			for (std::size_t i = 0; i < right_count; ++i)
-			{
-				if (it_rhs == end)
-				{
-					break;
-				}
-				++it_rhs;
-				if (sub_size == 1)
-				{
-					++size;
-				}
-			}
-
 			sub_size *= 2;
+			++count;
+			if (count > size / 10)
+			{
+				std::cout << '*';
+				count = 0;
+			}
+			if (_kbhit())
+			{
+				auto key = _getch();
+				if (key == 'c' || key == 'C')
+				{
+					stopped = true;
+				}
+			}
 		}
 		while (!stopped && sub_size < size);
+		std::cout << '*\n';
 	}
 
 	template <typename Iterator, typename Func>
